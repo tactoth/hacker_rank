ca6aa9cf0bb2b7eac3fe8f0c8ddb690f6cdaf734
Wei Liu <wei.liu@nobody.com>
2016-10-22 17:14:17 -0700

    sum pieces in order

--- src/main/scala/Solution.scala
+++ src/main/scala/Solution.scala
@@ -12,58 +12,97 @@ object Solution {
   }
 
 
-  def getOrElseUpdate[T](a: Array[T], index: Int, emptyValue: T)(compute: => T) = {
-    var value = a(index)
-    if (value == emptyValue) {
-      value = compute
-      a(index) = value
-    }
-    value
-  }
-
-  def getOrElseUpdate[T](a: Array[Array[T]], i: Int, j: Int, emptyValue: T)(compute: => T) = {
-    var value = a(i)(j)
-    if (value == emptyValue) {
-      value = compute
-      a(i)(j) = value
-    }
-    value
-  }
-
   val MOD = 1e9.asInstanceOf[Long] + 7
 
   private def _multi(a: Long, b: Long) = ((a % MOD) * (b % MOD)) % MOD // https://en.wikipedia.org/wiki/Modular_exponentiation
 
   private def _add(a: Long, b: Long) = (a + b) % MOD
 
-  private def _addSeq(a: Long*) = a.foldLeft(0L)(_add(_, _))
+  private def _addSeq(a: Long*) = {
+    var res = 0L
+    for (e <- a) {
+      res = _add(res, e)
+    }
+    res
+  }
 
-  private def _multiSeq(a: Long*) = a.foldLeft(1L)(_multi(_, _))
+  private def _multiSeq(a: Long*) = {
+    var res = 1L
+    for (e <- a) {
+      res = _multi(res, e)
+    }
+    res
+  }
+
+  class Power(base: Long, maxExp: Int) {
+    private val cache = Array.fill[Long](maxExp + 1)(-1)
+
+    def get(exp: Int): Long = {
+      if (cache(exp) != -1L)
+        cache(exp)
+      else {
+        val result = if (exp == 0)
+          1
+        else {
+          val half = exp / 2
+          _multiSeq(get(half), get(half), if (exp % 2 == 0) 1 else base)
+        }
+        cache(exp) = result
+        result
+      }
+    }
+  }
 
   class SP(a: Array[Long]) {
     private val length = a.length
-    private val d_ = Array.fill[Long](length + 1)(-1)
+    private val power2_ = new Power(2, length)
     private val sum_ = Array.fill[Long](length + 1)(-1)
     private val f_ = Array.fill[Long](length + 1, length + 1)(-1)
 
     // result are already mod
-    def d(l: Int): Long = getOrElseUpdate(d_, l, -1L) {
-      if (l == 0) 1 else if (l == 1) 1 else _add(_addSeq(Range(1, l).map(d(_)): _*), 1)
+    def d(l: Int): Long = {
+      if (l == 0 || l == 1)
+        1
+      else
+        power2_.get(l - 1)
     }
 
-    def sum(end: Int): Long = getOrElseUpdate(sum_, end, -1L) {
-      if (end == 0) 0 else sum(end - 1) + a(end - 1)
+    def sum(end: Int): Long = if (sum_(end) != -1) sum_(end)
+    else {
+      val result = if (end == 0) 0 else sum(end - 1) + a(end - 1)
+      sum_(end) = result
+      result
+    }
+
+    // pre compute d and sum
+    for (i <- Range.inclusive(0, length)) {
+      d(i)
+      sum(i)
     }
 
     def sum(s: Int, e: Int): Long = sum(e) - sum(s)
 
-    def f(s: Int, l: Int): Long = getOrElseUpdate(f_, s, l, -1L) {
-      (for (firstLength <- Range.inclusive(1, l)) yield {
-        _addSeq(_multiSeq(sum(s, s + firstLength), firstLength, d(l - firstLength)), f(s + firstLength, l - firstLength))
-      }).foldLeft(0L)(_add(_, _))
+    def f(s: Int, l: Int): Long = if (f_(s)(l) != -1) f_(s)(l)
+    else {
+      var all = 0L
+      for (firstLength <- Range.inclusive(1, l)) yield {
+        val sub = _addSeq(_multiSeq(sum(s, s + firstLength), firstLength, d(l - firstLength)), f(s + firstLength, l - firstLength))
+        all = _add(sub, all)
+      }
+      f_(s)(l) = all
+      all
     }
 
-    def get() = f(0, a.length)
+    def get() = {
+      // do this first to avoid recursive call
+      for (s <- Range.inclusive(length - 1, 0, -1)) {
+        f(s, length - s)
+      }
+
+      // eventually, we want this
+      val result = f(0, length)
+      result
+    }
   }
 
 }
--- src/main/scala/TestUtils.scala
+++ src/main/scala/TestUtils.scala
@@ -10,4 +10,19 @@ object TestUtils {
       println(s"Fail, input=$input, output=$output, expect=$expect")
     }
   }
+
+  var lastStart: Long = 0
+
+  def markStart(): Unit = {
+    lastStart = System.currentTimeMillis()
+  }
+
+  def reportDuration(label: String): Unit = {
+    val durationMs = System.currentTimeMillis() - lastStart
+    if (durationMs < 10000) {
+      Console.err.println(s"$label => $durationMs ms")
+    } else {
+      Console.err.println(s"$label => ${durationMs / 1000} s")
+    }
+  }
 }
