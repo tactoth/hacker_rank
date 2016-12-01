2793419b3cd6970689b0c67aeeb338bd18bba027
Liu Wei <tactoth@gmail.com>
2016-11-30 17:00:57 -0800

    finally the window range one works

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -5,21 +5,21 @@ public class Solution {
         if (nums.length < 2 || k == 0 || t < 0)
             return false;
 
-        TreeMap<Integer, Integer> lastIndex = new TreeMap<>();
+        TreeMap<Long, Integer> lastIndex = new TreeMap<>();
 
         int i = 0;
-        lastIndex.put(nums[0], 0);
+        lastIndex.put((long) nums[0], 0);
 
         for (int j = 1; j < nums.length; j++) {
-            while (j - i > k - 1) {
+            while (j - i > k) {
                 // ensure length is k + 1
-                if (lastIndex.get(nums[i]) == i)
-                    lastIndex.remove(nums[i]); // if i is the last, remove it
+                if (lastIndex.get((long) nums[i]) == i)
+                    lastIndex.remove((long) nums[i]); // if i is the last, remove it
                 i++;
             }
 
             // we'll add nums[j] to the range, see if there's any matches in the existing values
-            int e = nums[j];
+            long e = nums[j];
             if (!lastIndex.subMap(e - t, true, e + t, true).isEmpty())
                 return true;
 
@@ -27,5 +27,10 @@ public class Solution {
         }
 
         return false;
+    }
+
+    public static void main(String[] args) {
+        TestUtil.test(new Solution().containsNearbyAlmostDuplicate(new int[]{1, 2, 3}, 1, 1), true);
+        TestUtil.test(new Solution().containsNearbyAlmostDuplicate(new int[]{1, 8, 3}, 2, 2), true);
     }
 }
